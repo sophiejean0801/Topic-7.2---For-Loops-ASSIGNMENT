c3d25df06ba23441c33d55cb73d79fd3d32420cb
Sophie Jean <363449299@GoTVDSB.ca>
2025-04-01 13:45:38 -0400

    Add project files.

---
+++ Topic 7.2 - For Loops ASSIGNMENT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_7._2___For_Loops_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
